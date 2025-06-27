ae084da1a8e6ac80a748e28fb065d60393c53daf
agui <167501331+isntgui@users.noreply.github.com>
2025-06-27 00:49:17 -0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 #define int int64_t
+#define dbg(v) cerr << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << "\n"
 
 int32_t main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    ;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	;
 }
