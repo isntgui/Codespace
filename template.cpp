6e5abba892ab045a41261b2c2e35300efe112955
agui <167501331+isntgui@users.noreply.github.com>
2025-12-10 07:45:22 -0400

    Update template.cpp with new main structure
    Refactor main function and add utility macros

--- template.cpp
+++ template.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int int64_t
-#define dbg(v) cerr << "Line(" << __LINE__ << ") -> " << #v << " = " << (v) << "\n"
+#ifdef o_0
+    #include "debugging.h"
+#endif
 
-int32_t main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
-	;
+using ll = long long int;
+
+#define sum accumulate
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ;
 }
