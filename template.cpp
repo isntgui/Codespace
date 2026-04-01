39c8ffecd13b8860694d81584da89ccc2c95c1a2
agui <167501331+isntgui@users.noreply.github.com>
2026-04-01 14:14:58 -0400

    Refactor template.cpp for better compatibility

--- template.cpp
+++ template.cpp
@@ -1,18 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#ifdef o_0
-    #include "debugging.h"
-#endif
+#define int long long
 
-using ll = long long int;
-
-#define sum accumulate
-#define all(a) a.begin(), a.end()
-#define rall(a) a.rbegin(), a.rend()
-
-int main() {
+int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    ;
+    
 }
