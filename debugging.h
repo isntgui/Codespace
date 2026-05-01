37a791e8f2e1b901e096c0ebf7523b29e2c57a6d
agui <167501331+isntgui@users.noreply.github.com>
2026-04-30 21:42:43 -0400

    Refactor debugging.h for improved output stream support

--- debugging.h
+++ debugging.h
@@ -1,180 +1,89 @@
-#include <iostream>
-#include <vector>
-#include <deque>
-#include <map>
-#include <unordered_map>
-#include <set>
-#include <unordered_set>
-#include <queue>
-#include <stack>
-
-template <typename T> 
-std::ostream& operator<<(std::ostream& out, const std::stack<T>& s) {
-    std::stack<T> temp = s;
-    out << "[";
-    while(!temp.empty()) {
-        out << temp.top();
-        temp.pop();
-        out << (temp.empty()?"":", ");
-    }
-    out << "]";
-    return out;
-}
-
-template <typename T> 
-std::ostream& operator<<(std::ostream& out, const std::queue<T>& q) {
-    std::queue<T> temp = q;
-    out << "[";
-    while(!temp.empty()) {
-        out << temp.front();
-        temp.pop();
-        out << (temp.empty()?"":", ");
-    }
-    out << "]";
-    return out;
-}
-
-template <typename T> 
-std::ostream& operator<<(std::ostream& out, const std::priority_queue<T>& pq) {
-    std::priority_queue<T> temp = pq;
-    out << "[";
-    while(!temp.empty()) {
-        out << temp.top();
-        temp.pop();
-        out << (temp.empty()?"":", ");
-    }
-    out << "]";
-    return out;
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec) {
-    if (vec.empty()) {
-        out << "[]";
-        return out;
-    }
-    out << '[';
-    for (int i = 0; i < vec.size() - 1; i++) {
-        out << vec[i] << ", ";
-    }
-    return out << vec.back() << ']';
-}
+#include <bits/stdc++.h>
+using namespace std;
 
 template <typename T1, typename T2>
-std::ostream& operator<<(std::ostream& out, const std::pair<T1, T2>& pair) {
-    return out << '(' << pair.first << ", " << pair.second << ')';
+ostream& operator<<(ostream& out, const pair<T1, T2>& p) {
+    return out << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T, typename = void>
+struct is_iterable : false_type {};
+
+template <typename T>
+struct is_iterable<
+    T,
+    void_t<decltype(begin(declval<T>())), decltype(end(declval<T>()))> > : true_type {};
+
+template <>
+struct is_iterable<string> : false_type {};
+
+template <typename T>
+struct is_special_container : false_type {};
+
+template <typename... Args>
+struct is_special_container<stack<Args...> > : true_type {};
+
+template <typename... Args>
+struct is_special_container<queue<Args...> > : true_type {};
+
+template <typename... Args>
+struct is_special_container<priority_queue<Args...> > : true_type {};
+
+template <typename T>
+enable_if_t<
+    is_iterable<T>::value && !is_special_container<T>::value,
+    ostream&>
+operator<<(ostream& out, const T& container) {
+    out << "[";
+    auto it = begin(container);
+    while (it != end(container)) {
+        out << *it;
+        ++it;
+        if (it != end(container))
+            out << ", ";
+    }
+    return out << "]";
+}
+
+template <typename Tuple, size_t... Is>
+void print_tuple(ostream& out, const Tuple& t, index_sequence<Is...>) {
+    ((out << (Is ? ", " : "") << get<Is>(t)), ...);
+}
+
+template <typename... Args>
+ostream& operator<<(ostream& out, const tuple<Args...>& t) {
+    out << "(";
+    print_tuple(out, t, index_sequence_for<Args...>{});
+    return out << ")";
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::deque<T>& deq) {
-    if (deq.empty()) {
-        out << "[]";
-        return out;
+ostream& operator<<(ostream& out, stack<T> s) {
+    vector<T> v;
+    while (!s.empty()) {
+        v.push_back(s.top());
+        s.pop();
     }
-    out << '[';
-    for (int i = 0; i < deq.size() - 1; i++) {
-        out << deq[i] << ", ";
-    }
-    return out << deq.back() << ']';
-}
-
-template <typename T1, typename T2>
-std::ostream& operator<<(std::ostream& out, const std::unordered_map<T1, T2>& map) {
-    out << '{';
-    for (auto it = map.begin(); it != map.end(); it++) {
-        std::pair<T1, T2> element = *it;
-        out << element.first << ": " << element.second;
-        if (std::next(it) != map.end()) {
-            out << ", ";   
-        }
-    }
-    return out << '}';
-}
-
-template <typename T1, typename T2>
-std::ostream& operator<<(std::ostream& out, const std::map<T1, T2>& map) {
-    out << '{';
-    for (auto it = map.begin(); it != map.end(); it++) {
-        std::pair<T1, T2> element = *it;
-        out << element.first << ": " << element.second;
-        if (std::next(it) != map.end()) {
-            out << ", ";   
-        }
-    }
-    return out << '}';
+    reverse(v.begin(), v.end());
+    return out << v;
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::unordered_set<T>& set) {
-    out << '{';
-    for (auto it = set.begin(); it != set.end(); it++) {
-        T element = *it;
-        out << element;
-        if (std::next(it) != set.end()) {
-            out << ", ";   
-        }
+ostream& operator<<(ostream& out, queue<T> q) {
+    vector<T> v;
+    while (!q.empty()) {
+        v.push_back(q.front());
+        q.pop();
     }
-    return out << '}';
+    return out << v;
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::multiset<T>& set) {
-    out << '{';
-    for (auto it = set.begin(); it != set.end(); it++) {
-        T element = *it;
-        out << element;
-        if (std::next(it) != set.end()) {
-            out << ", ";   
-        }
+ostream& operator<<(ostream& out, priority_queue<T> pq) {
+    vector<T> v;
+    while (!pq.empty()) {
+        v.push_back(pq.top());
+        pq.pop();
     }
-    return out << '}';
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::unordered_multiset<T>& set) {
-    out << '{';
-    for (auto it = set.begin(); it != set.end(); it++) {
-        T element = *it;
-        out << element;
-        if (std::next(it) != set.end()) {
-            out << ", ";   
-        }
-    }
-    return out << '}';
-}
-
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const std::set<T>& set) {
-    out << '{';
-    for (auto it = set.begin(); it != set.end(); it++) {
-        T element = *it;
-        out << element;
-        if (std::next(it) != set.end()) {
-            out << ", ";   
-        }
-    }
-    return out << '}';
-}
-
-// Source: https://stackoverflow.com/a/31116392/12128483
-template<typename Type, unsigned N, unsigned Last>
-struct TuplePrinter {
-    static void print(std::ostream& out, const Type& value) {
-        out << std::get<N>(value) << ", ";
-        TuplePrinter<Type, N + 1, Last>::print(out, value);
-    }
-};
-
-template<typename Type, unsigned N>
-struct TuplePrinter<Type, N, N> {
-    static void print(std::ostream& out, const Type& value) {
-        out << std::get<N>(value);
-    }
-};
-
-template<typename... Types>
-std::ostream& operator<<(std::ostream& out, const std::tuple<Types...>& value) {
-    out << '(';
-    TuplePrinter<std::tuple<Types...>, 0, sizeof...(Types) - 1>::print(out, value);
-    return out << ')';
+    return out << v;
 }
